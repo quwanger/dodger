243abc86e3be7b3b0509422932a4e3a2576ca0e2
Quang Nguyen <quwanger@gmail.com>
2018-01-31 16:58:50 -0500

    Adding players and changing point values

---
+++ js/app.js
@@ -0,0 +1,59 @@
+var app = new Vue({
+	el: '#dodger',
+	data: {
+		newPlayer: '',
+		players: [],
+		title: 'Title'
+	},
+
+	methods: {
+		addPlayer() {
+			if (this.newPlayer.length > 0) {
+				this.players.push({ name: this.newPlayer, kills: 0, deaths: 0, catches: 0 });
+				this.newPlayer = '';
+			} else {
+				alert('Add a name!');
+			}	
+		},
+
+		addPoints: function(key, index) {
+			switch(key) {
+				case 'kills':
+					this.players[index].kills++;
+					break;
+				case 'deaths':
+					this.players[index].deaths++;
+					break;
+				case 'catches':
+					this.players[index].catches++;
+					break;
+			}
+        },
+
+        minusPoints: function(key, index) {
+    		switch(key) {
+				case 'kills':
+					if (this.players[index].kills > 0)
+						this.players[index].kills--;
+					break;
+				case 'deaths':
+					if (this.players[index].deaths > 0)
+						this.players[index].deaths--;
+					break;
+				case 'catches':
+					if (this.players[index].catches > 0)
+						this.players[index].catches--;
+					break;
+			}
+        },
+	},
+
+	computed: {
+
+	},
+
+	// Runs when the Vue object is loaded
+	mounted() {
+		
+	}
+});
